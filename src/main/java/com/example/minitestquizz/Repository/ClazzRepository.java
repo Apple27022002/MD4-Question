3fc22a8aa11ef05822e3f7d2cb4984c8e1e15cc6
Apple27022002 <100838894+Apple27022002@users.noreply.github.com>
2022-06-29 09:29:18 +0700

    test

--- src/main/java/com/example/minitestquizz/Repository/ClazzRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.minitestquizz.Repository;
-
-import com.example.minitestquizz.Model.Clazz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClazzRepository extends JpaRepository<Clazz,Long> {
-}
